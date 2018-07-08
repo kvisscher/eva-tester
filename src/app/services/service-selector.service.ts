ec8ac03492fd2b3756afb07b90dcec4d14929522
Abdel El-Medny <abdel@springtree.nl>
2018-07-08 15:59:07 +0200

    feat(service-selector): adds service selector

---
+++ src/app/services/service-selector.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * The side menu showing all the available services needs to have a way to communicate with the active tab
+ * */
+@Injectable()
+export class ServiceSelectorService {
+
+  constructor() { }
+}
