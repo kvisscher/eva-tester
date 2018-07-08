6b5cfcbc29de995aabed7a6e1e6e1fd17da73fe5
Abdel El-Medny <abdel@springtree.nl>
2018-07-08 15:26:12 +0200

    feat(routing): update module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TesterComponent } from './pages/tester/tester.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TesterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
