446ffaa832f37db7b3de0d24a2aa2d1f99d5c9bc
Abdel El-Medny <abdel@springtree.nl>
2018-07-16 09:53:20 +0200

    feat(tester): adds initial version of layout

--- src/app/pages/tester/tester.component.ts
+++ src/app/pages/tester/tester.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AngularFusejsOptions } from 'angular-fusejs';
+import { debounceTime } from '../../../../node_modules/rxjs/operators';
+import { ListServicesService } from '../../services/list-services.service';
 
 @Component({
   selector: 'eva-tester',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TesterComponent implements OnInit {
 
-  constructor() { }
+  public readonly services$ = this.$listServices.services$;
+
+  public searchOptions: AngularFusejsOptions = {
+    keys: ['name'],
+    maximumScore: 0.5
+  };
+
+  public searchForm = this.formBuilder.group({
+    search: [null]
+  });
+
+  searchTerms: string;
+
+  constructor(
+    private $listServices: ListServicesService,
+    private formBuilder: FormBuilder
+  ) {
+    this.$listServices.fetch();
+
+    this.searchForm.get('search').valueChanges.pipe(debounceTime(500)).subscribe( (value: string) => {
+      this.searchTerms = value;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  selectService() {
+
   }
 
 }
