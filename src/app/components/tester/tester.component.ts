154a886789300f99ae9e70560065bcdd7183cafd
Abdel El-Medny <abdel@springtree.nl>
2018-07-26 14:14:25 +0200

    feat(tester): compile ts to js

--- src/app/components/tester/tester.component.ts
+++ src/app/components/tester/tester.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { AngularFusejsOptions } from 'angular-fusejs';
 import { debounceTime } from '../../../../node_modules/rxjs/operators';
-import { ListServicesService } from '../../services/list-services.service';
 import { NgxEditorModel } from '../../components/editor';
 import { EvaTypingsService } from '../../services/eva-typings.service';
+import { ListServicesService, IService } from '../../services/list-services.service';
+import { ServiceSelectorService } from '../../services/service-selector.service';
+import { EditorComponent } from '../editor/editor.component';
 
 @Component({
   selector: 'eva-tester',
@@ -12,6 +14,8 @@ import { EvaTypingsService } from '../../services/eva-typings.service';
   styleUrls: ['./tester.component.scss']
 })
 export class TesterComponent implements OnInit {
+
+  @ViewChild(EditorComponent) monacoEditor: EditorComponent;
 
   public readonly services$ = this.$listServices.services$;
 
@@ -24,41 +28,47 @@ export class TesterComponent implements OnInit {
     search: [null]
   });
 
-  public code = [
-    '({',
-    `  LanguageID: '1'`,
-    '} as Partial<EVA.Core.Services.UpdateUser>);'
-  ].join('\n');
+  public uniqueURI = `index-${Math.random()}.ts`;
 
   public monacoModel: NgxEditorModel = {
-    value: this.code,
+    value: this.createCodeTemplate('EVA.Core.Services.UpdateUser'),
     language: 'typescript',
-    uri: 'index.ts'
+    uri: this.uniqueURI
   };
 
   public monacoOptions = {
     theme: 'vs-dark'
   };
 
+  public compileOutput: string;
+
   searchTerms: string;
 
   constructor(
     private $listServices: ListServicesService,
     private formBuilder: FormBuilder,
-    private $evaTypings: EvaTypingsService
+    private $evaTypings: EvaTypingsService,
+    private $serviceSelector: ServiceSelectorService
   ) {
-    this.$listServices.fetch();
-
     this.searchForm.get('search').valueChanges.pipe(debounceTime(500)).subscribe( (value: string) => {
       this.searchTerms = value;
     });
   }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.monacoModel.value = 'fa;ljfakljfakl';
+      console.log('override');
+    }, 3000);
   }
 
-  selectService() {
+  /** Whenever a service is selected, we will fetch it and create a code template */
+  selectService(service: IService) {
+    this.$serviceSelector.fetch(service.type).subscribe( (value: any) => {
+      const newEditorValue = this.createCodeTemplate(value.request.ns + '.' + value.request.type);
 
+      this.monacoEditor.writeValue(newEditorValue);
+    });
   }
 
 
@@ -85,21 +95,59 @@ export class TesterComponent implements OnInit {
     });
   }
 
-  compileEditorInput() {
+  async compileEditorInput() {
 
-    const models = monaco.editor.getModels();
+    const model = monaco.editor.getModels()
+      .find( potentialMatchingModel => potentialMatchingModel.uri.toString() === this.uniqueURI );
+    /** @see https://github.com/Microsoft/monaco-typescript/pull/8 */
+    const worker = await monaco.languages.typescript.getTypeScriptWorker();
 
-    const model = models[0];
+    const client = await worker(model.uri);
 
-    monaco.languages.typescript.getTypeScriptWorker().then(worker => {
-      worker(model).then(client => {
-        console.log(client);
+    const output = await client.getEmitOutput(model.uri.toString());
 
-        const output = client.getEmitOutput(model.uri.toString()).then(console.log);
+    const matchingOutput: { name: string, text: string } = output.outputFiles.find( potentialMatchingOutput => {
+      /** transforming ts uri to js because output files will be javascript files */
+      const jsURI = this.uniqueURI.replace('ts', 'js');
+      const match = potentialMatchingOutput.name === jsURI;
 
-        console.log(output);
-      });
+      return match;
     });
+
+    console.log(matchingOutput);
+
+    const codeWithoutParentheses = matchingOutput.text.replace('(', '').replace(')', '');
+
+    /**
+     * we want to split the code on new line and replace the first line with a {
+     * and remove the rest 'const request' part
+     */
+    const splitCode = matchingOutput.text.split('\n');
+
+    // Replacing the first line with an opening brace
+    splitCode[0] = '{';
+
+    // Joining the js object array and removing the semicolon so its valid json
+    //
+    const jsObject: string = splitCode.join('').replace(';', '');
+
+    // tslint:disable-next-line:no-eval
+    const jsonObject: Object = eval(`(${jsObject})`);
+
+    const jsonString: string = JSON.stringify(jsonObject);
+
+    this.compileOutput = jsonString;
+  }
+
+  /** Returns the template that casts an empty object to a given eva type */
+  createCodeTemplate(requestType: string): string {
+    const codeTemplate = [
+      `const request: Partial<${requestType}> = {`,
+      '  ',
+      `}`
+    ].join('\n');
+
+    return codeTemplate;
   }
 
 }
