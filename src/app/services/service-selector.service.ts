7ae91951759428f7bc43db291db6566ac461a070
Abdel El-Medny <abdel@springtree.nl>
2018-07-28 19:05:06 +0200

    feat(service-selector): add resposne typings

--- src/app/services/service-selector.service.ts
+++ src/app/services/service-selector.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TYPINGS_END_POINT } from './eva-typings.service';
+import { IListServiceItem } from './list-services.service';
+export interface IServiceResponse {
+  description: string;
+  request: IServiceResponseField;
+  response: IServiceResponseField;
+  routes: {
+    method: 'POST' | 'GET' | 'PUT' | 'DELETE';
+    path: string;
+  };
+  security: {
+    requiredFunctionalities: Array<{
+      functionality: string;
+      scope: string
+    }>,
+    requiresAuthentication: boolean;
+    requiredUserType: string[]
+  };
+}
+interface IServiceResponseField extends IListServiceItem {
+  description: string;
+  fields: IServiceResponseField[];
+  optional: boolean;
+}
 
 @Injectable()
 export class ServiceSelectorService {
@@ -8,6 +31,6 @@ export class ServiceSelectorService {
   constructor(private http: HttpClient ) { }
 
   fetch( serviceType: string ) {
-    return this.http.get(TYPINGS_END_POINT + '/tester/api/services/' + serviceType);
+    return this.http.get<IServiceResponse>(TYPINGS_END_POINT + '/tester/api/services/' + serviceType);
   }
 }
