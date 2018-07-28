97cfe3f45efe1a436528de5775b288e3c0fdcf44
Abdel El-Medny <abdel@springtree.nl>
2018-07-29 00:36:16 +0200

    feat(markdown-html): adds highlight feature of existing services

--- src/app/components/markdown-html/markdown-html.component.ts
+++ src/app/components/markdown-html/markdown-html.component.ts
@@ -1,12 +1,59 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, Injectable, OnDestroy } from '@angular/core';
 import * as showdown from 'showdown';
+import { ListServicesService } from '../../services/list-services.service';
+import { first, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+
+@Injectable()
+class EvaServiceHighLighter implements OnDestroy {
+
+  codeElementsMap = new Map<HTMLElement, EventListenerOrEventListenerObject>();
+
+  constructor(protected $listServices: ListServicesService, protected router: Router) { }
+
+  /**
+   * The docs tend to refer to other services in <code> </code> blocks
+   * We will query all elements in the generated markdown, checkout all the code nodes and see if they are referring to any services
+   */
+  async highlightPotentialServices(el: HTMLElement) {
+
+    const serviceNames = await this.$listServices.services$.pipe(
+      map( values => values.map( service => service.name ) ),
+      first()
+    ).toPromise();
+
+    Array.from(el.querySelectorAll('code')).filter( codeElement => {
+      return serviceNames.includes(codeElement.innerText);
+    })
+    .forEach( codeElement => {
+
+      codeElement.style.cursor = 'pointer';
+
+      const handler = () => {
+        this.router.navigate(['/service/' + codeElement.innerText]);
+      };
+
+      codeElement.addEventListener('click', handler);
+
+      this.codeElementsMap.set(codeElement, handler);
+    });
+  }
+
+
+  ngOnDestroy() {
+    Array.from(this.codeElementsMap).forEach( ([codeElement, listener]) => {
+      codeElement.removeEventListener('click', listener);
+    });
+  }
+}
 
 @Component({
   selector: 'eva-markdown-html',
   templateUrl: './markdown-html.component.html',
   styleUrls: ['./markdown-html.component.scss']
 })
-export class MarkdownHtmlComponent implements OnInit {
+export class MarkdownHtmlComponent extends EvaServiceHighLighter implements OnInit  {
 
   private converter = new showdown.Converter();
 
@@ -23,7 +70,9 @@ export class MarkdownHtmlComponent implements OnInit {
     this._markdown = markdown;
   }
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, protected $listServices: ListServicesService, protected router: Router) {
+    super($listServices, router);
+   }
 
   ngOnInit() {
 
@@ -33,6 +82,7 @@ export class MarkdownHtmlComponent implements OnInit {
     const html = this.converter.makeHtml(markdown);
 
     (this.el.nativeElement as HTMLElement).innerHTML = html;
-  }
 
+    this.highlightPotentialServices(this.el.nativeElement);
+  }
 }
