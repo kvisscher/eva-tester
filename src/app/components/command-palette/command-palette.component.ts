0e105c19e15e874a7aa030e99487566273760ace
Abdel El-Medny <abdel@springtree.nl>
2018-07-28 20:46:27 +0200

    feat(command-palette): adds initial version of command palette

--- src/app/components/command-palette/command-palette.component.ts
+++ src/app/components/command-palette/command-palette.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, NgZone } from '@angular/core';
 import { Logger, ILoggable } from '../../decorators/logger';
+import { FormBuilder } from '@angular/forms';
+import { fadeInOut } from '../../shared/animations';
+import { first } from 'rxjs/operators';
 
 @Logger
 @Component({
   selector: 'eva-command-palette',
   templateUrl: './command-palette.component.html',
-  styleUrls: ['./command-palette.component.scss']
+  styleUrls: ['./command-palette.component.scss'],
+  animations: [ fadeInOut ]
 })
 export class CommandPaletteComponent implements OnInit, ILoggable {
-  public logger: Partial<Console>;
+  @ViewChild('input') input: { nativeElement: HTMLInputElement };
 
-  constructor() { }
+  logger: Partial<Console>;
+
+  show = false;
+
+  public tags = ['organization unit', 'users', 'products', 'orders'];
+
+  public autocomplete: string;
+
+  public form = this.fb.group({
+    search: []
+  });
+
+  constructor(private fb: FormBuilder, private zone: NgZone) {
+    this.form.get('search').valueChanges
+      .subscribe(value => {
+        if (Boolean(value)) {
+          const matchingTags = this.tags.filter(tag => {
+            const matchingTag = tag.includes(value);
+            return matchingTag;
+          });
+          this.autocomplete = matchingTags[0];
+        } else {
+          this.autocomplete = null;
+        }
+      });
+  }
 
   ngOnInit() {
   }
@@ -18,13 +47,26 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
   @HostListener('window:keyup.shift.p', ['$event']) shiftP(e: KeyboardEvent) {
     e.preventDefault();
 
-    this.logger.log('open spotlight');
+    console.log('open spotlight');
+
+    this.show = true;
+
+    this.zone.onStable.pipe(first()).subscribe(() => {
+      this.input.nativeElement.focus();
+    });
   }
 
   @HostListener('window:keyup.Escape', ['$event']) esc(e: KeyboardEvent) {
     e.preventDefault();
 
-    this.logger.log('close spotlight');
+    console.log('close spotlight');
+
+    this.show = false;
+  }
+
+  tabPressed(e: KeyboardEvent) {
+    e.preventDefault();
+    console.log('pressed');
   }
 
 }
