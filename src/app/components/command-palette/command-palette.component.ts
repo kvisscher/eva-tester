5773d46b89e3666720e6e3d07512e09ce533b0ed
Abdel El-Medny <abdel@springtree.nl>
2018-07-31 23:11:51 +0200

    feat(command-palette): makes organizations searchable

--- src/app/components/command-palette/command-palette.component.ts
+++ src/app/components/command-palette/command-palette.component.ts
@@ -1,21 +1,26 @@
-import { Component, HostListener, NgZone, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, NgZone, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { searchOrders, searchProducts, searchUsers, store, getProductDetail } from '@springtree/eva-sdk-redux';
-import { AngularFusejsOptions } from 'angular-fusejs';
+import { searchOrders, searchProducts, searchUsers, store, getProductDetail, getOrganizationUnitsForUser } from '@springtree/eva-sdk-redux';
+import { AngularFusejsOptions, FusejsPipe } from 'angular-fusejs';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { debounceTime, filter, first, map } from 'rxjs/operators';
 import { ILoggable, Logger } from '../../decorators/logger';
 import { fadeInOut, slideUpDown } from '../../shared/animations';
 import isNotNil from '../../shared/operators/is-not-nil';
+import { OrganizationSelectorComponent } from '../organization-selector/organization-selector.component';
+import { isEmpty } from 'lodash';
+import { EndPointUrlService } from '../../services/end-point-url.service';
 
-enum CurrentSearchType {
+enum CurrentAction {
   MAINSEARCH = 0,
   PRODUCTS = 1,
   ORDERS = 2,
   USERS = 3,
-  PRODUCTBARCODE = 4
+  PRODUCTBARCODE = 4,
+  ORGANIZATIONS = 5,
+  ENDPOINTURL = 6
 }
 
 interface IEvaSearchResult {
@@ -29,14 +34,32 @@ interface IEvaSearchResult {
   selector: 'eva-command-palette',
   templateUrl: './command-palette.component.html',
   styleUrls: ['./command-palette.component.scss'],
-  animations: [fadeInOut, slideUpDown]
+  animations: [fadeInOut, slideUpDown],
+  providers: [OrganizationSelectorComponent, FusejsPipe]
 })
 export class CommandPaletteComponent implements OnInit, ILoggable {
   @ViewChild('input') mainSearchInput: { nativeElement: HTMLInputElement };
 
+  @ViewChild('results') resultsContainer: { nativeElement: HTMLDivElement };
+
   logger: Partial<Console>;
 
-  show = false;
+  private _show = false;
+
+  public get show() {
+    return this._show;
+  }
+  public set show(show) {
+    this._show = show;
+
+
+    if ( show === false ) {
+      this.currentAction = CurrentAction.MAINSEARCH;
+      this.form.reset();
+    }
+  }
+
+  public activeSearchIndex = 0;
 
   public searchOptions: AngularFusejsOptions = {
     keys: ['title'],
@@ -48,10 +71,12 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
   };
 
   public actions = [
-    { title: 'Search: Products', value: CurrentSearchType.PRODUCTS },
-    { title: 'Search: Orders', value: CurrentSearchType.ORDERS },
-    { title: 'Search: Users', value: CurrentSearchType.USERS },
-    { title: 'Search: Product barcode', value: CurrentSearchType.PRODUCTBARCODE }
+    { title: 'Search: Products', value: CurrentAction.PRODUCTS },
+    { title: 'Search: Orders', value: CurrentAction.ORDERS },
+    { title: 'Search: Users', value: CurrentAction.USERS },
+    { title: 'Search: Product barcode', value: CurrentAction.PRODUCTBARCODE },
+    { title: 'Select: Organization unit', value: CurrentAction.ORGANIZATIONS },
+    { title: 'Update: End point url ', value: CurrentAction.ENDPOINTURL }
   ];
 
   public form = this.fb.group({
@@ -65,45 +90,52 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
   /** This is a reflection of the mainSearch control but debounced */
   public mainSearchTerms: Observable<string> = this.form.get('mainSearch').valueChanges
     .pipe(
-      filter(() => this.currentSearchEnum === CurrentSearchType.MAINSEARCH),
+      filter(() => this.currentAction === CurrentAction.MAINSEARCH),
       debounceTime(300)
     );
 
   public get showMainSearchResults(): boolean {
     const value: string = this.form.get('mainSearch').value as string || '';
 
-    const showMainSearchResults = value.length > 0 && this.currentSearchEnum === CurrentSearchType.MAINSEARCH;
+    const showMainSearchResults = value.length > 0 && this.currentAction === CurrentAction.MAINSEARCH;
 
     return showMainSearchResults;
   }
 
   public get showEvaResults(): boolean {
-    return this.currentSearchEnum !== CurrentSearchType.MAINSEARCH;
+    return this.currentAction !== CurrentAction.MAINSEARCH;
   }
 
   public get searchPlaceHolder(): string {
 
     let placeholder = '';
 
-    if (this.currentSearchEnum === CurrentSearchType.ORDERS) {
+    if (this.currentAction === CurrentAction.ORDERS) {
       placeholder = 'Search orders';
     }
-    if (this.currentSearchEnum === CurrentSearchType.PRODUCTS) {
+    if (this.currentAction === CurrentAction.PRODUCTS) {
       placeholder = 'Search products';
     }
-    if (this.currentSearchEnum === CurrentSearchType.USERS) {
+    if (this.currentAction === CurrentAction.USERS) {
       placeholder = 'Search users';
+    }
+    if ( this.currentAction === CurrentAction.ORGANIZATIONS ) {
+      placeholder = 'Select organization';
     }
 
     return placeholder;
   }
 
-  currentSearchEnum: CurrentSearchType = CurrentSearchType.MAINSEARCH;
+  currentAction: CurrentAction = CurrentAction.MAINSEARCH;
 
   constructor(
     private fb: FormBuilder,
     private zone: NgZone,
-    private matSnackBar: MatSnackBar
+    private matSnackBar: MatSnackBar,
+    private organizationSelectorComponent: OrganizationSelectorComponent,
+    private fusejsPipe: FusejsPipe,
+    private el: ElementRef,
+    private $endPointUrlService: EndPointUrlService
   ) { }
 
   ngOnInit() {
@@ -114,7 +146,7 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
         debounceTime(300)
       )
       .subscribe(query => {
-        if (this.currentSearchEnum === CurrentSearchType.USERS) {
+        if (this.currentAction === CurrentAction.USERS) {
           const [action] = searchUsers.createFetchAction({
             SearchQuery: query
           });
@@ -133,7 +165,7 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
 
           this.evaSearchResultLoading$ = searchUsers.isFetching$();
         }
-        if (this.currentSearchEnum === CurrentSearchType.PRODUCTS || this.currentSearchEnum === CurrentSearchType.PRODUCTBARCODE) {
+        if (this.currentAction === CurrentAction.PRODUCTS || this.currentAction === CurrentAction.PRODUCTBARCODE) {
           const [action] = searchProducts.createFetchAction({
             Query: query,
             IncludedFields: ['display_value', 'product_id', 'display_price', 'backend_id', 'barcodes']
@@ -142,18 +174,29 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
           this.evaSearchResults$ = searchProducts.getResponse$().pipe(
             isNotNil(),
             map(res => res.Products),
-            map(page => page.map(product => ({
-              id: product.product_id,
-              rawObject: product,
-              title: product.display_value
-            } as IEvaSearchResult)))
+            map(page => {
+              let productResults = page
+                .map(product => ({
+                  id: product.product_id,
+                  rawObject: product,
+                  title: product.display_value
+                } as IEvaSearchResult));
+
+              // If we are in barcode mode, we will filter out products without a filled barcodes array
+              //
+              if ( this.currentAction === CurrentAction.PRODUCTBARCODE ) {
+                productResults = productResults.filter( product => isEmpty(product.rawObject.barcodes) === false );
+              }
+
+              return productResults;
+            })
           );
 
           this.evaSearchResultLoading$ = searchProducts.isFetching$();
 
           store.dispatch(action);
         }
-        if (this.currentSearchEnum === CurrentSearchType.ORDERS) {
+        if (this.currentAction === CurrentAction.ORDERS) {
           const [action] = searchOrders.createFetchAction({
             Query: query
           });
@@ -172,10 +215,29 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
 
           store.dispatch(action);
         }
+        if (this.currentAction === CurrentAction.ORGANIZATIONS) {
+          this.evaSearchResults$ = getOrganizationUnitsForUser.getResponse$().pipe(
+            isNotNil(),
+            map( res => res.Result ),
+            map(page => {
+              const organizations: IEvaSearchResult[] = page.map( organization => {
+                return {
+                  id: organization.ID,
+                  title: organization.Name,
+                  rawObject: organization
+                } as IEvaSearchResult;
+              });
+
+              const fusejsSortedOrganizations = this.fusejsPipe.transform(organizations, query, this.searchOptions);
+
+              return fusejsSortedOrganizations;
+            })
+          );
+        }
       });
   }
 
-  @HostListener('window:keyup.shift.p', ['$event']) shiftP(e: KeyboardEvent) {
+  @HostListener('window:keyup.shift.p', ['$event']) onShiftP(e: KeyboardEvent) {
     e.preventDefault();
 
     console.log('open spotlight');
@@ -187,22 +249,70 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
     });
   }
 
-  @HostListener('window:keyup.Escape', ['$event']) esc(e: KeyboardEvent) {
+  @HostListener('window:keydown.ArrowUp', ['$event']) onArrowUp(e: KeyboardEvent) {
     e.preventDefault();
 
-    console.log('close spotlight');
+    const resultElements = this.resultsContainer.nativeElement.querySelectorAll('a');
 
-    this.show = false;
+    if ( this.activeSearchIndex === 0 ) {
+      const newIndex = resultElements.length - 1;
 
-    this.currentSearchEnum = CurrentSearchType.MAINSEARCH;
+      this.activeSearchIndex = newIndex;
+    } else {
+      this.activeSearchIndex--;
+    }
 
-    this.form.reset();
+    resultElements[this.activeSearchIndex].scrollIntoView({ behavior: 'smooth' });
+
+    resultElements[this.activeSearchIndex].focus();
   }
 
-  selectMainSearchOpt(e: KeyboardEvent | MouseEvent, value: CurrentSearchType) {
+  @HostListener('window:keydown.ArrowDown', ['$event']) onArrowDown(e: KeyboardEvent) {
     e.preventDefault();
 
-    this.currentSearchEnum = value;
+    const resultElements = this.resultsContainer.nativeElement.querySelectorAll('a');
+
+    if ( resultElements.length - 1 === this.activeSearchIndex ) {
+      this.activeSearchIndex = 0;
+    } else {
+      this.activeSearchIndex++;
+    }
+
+    resultElements[this.activeSearchIndex].scrollIntoView({ behavior: 'smooth' });
+
+    resultElements[this.activeSearchIndex].focus();
+  }
+
+  @HostListener('window:keyup.Escape', ['$event']) onEscape(e: KeyboardEvent) {
+    e.preventDefault();
+    this.show = false;
+  }
+
+
+  @HostListener('document:click', ['$event']) onClick(event: MouseEvent) {
+    /**
+     * The path of the event which will help us see what was clicked
+     * We also will check if the element has a nodename because window comes back as part of the path
+     */
+    const eventPath = ((event as any).composedPath() as HTMLElement[])
+      .filter( element => element.nodeName );
+
+    /** Whether this component was clicked or not */
+    const commandPaletteComponentClicked: HTMLElement | null = eventPath.find( element => {
+      return this.el.nativeElement === element;
+    });
+
+    if ( Boolean(commandPaletteComponentClicked) === false ) {
+      this.show = false;
+    }
+  }
+
+  selectMainSearchOpt(e: KeyboardEvent | MouseEvent, value: CurrentAction) {
+    e.preventDefault();
+
+    this.currentAction = value;
+
+    this.activeSearchIndex = null;
 
     this.form.get('mainSearch').reset();
 
@@ -216,15 +326,17 @@ export class CommandPaletteComponent implements OnInit, ILoggable {
 
     this.show = false;
 
-    switch (this.currentSearchEnum) {
-      case CurrentSearchType.PRODUCTS:
-      case CurrentSearchType.ORDERS:
-      case CurrentSearchType.USERS:
+    switch (this.currentAction) {
+      case CurrentAction.PRODUCTS:
+      case CurrentAction.ORDERS:
+      case CurrentAction.USERS:
         this.copyEvaSearchResult(value);
         break;
-      case CurrentSearchType.PRODUCTBARCODE:
-        /* Get product detail and copy to clipboard */
+      case CurrentAction.PRODUCTBARCODE:
         this.copyProductBarcode(value);
+        break;
+      case CurrentAction.ORGANIZATIONS:
+        this.organizationSelectorComponent.switchOrganization(value.id);
         break;
     }
    }
